04a28ed4dfa7a71461d2afd395954c30846a0e15
Vloker <agimgimnastiar09@gmail.com>
2024-06-15 06:50:36 +0800

    update

--- src/components/ButtonThread.jsx
+++ src/components/ButtonThread.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom'
 
 function ButtonThread() {
 
   const handleClick = () => {
-    Navigate('/thread')
+    window.location.href = '/Thread'
   }
 
   return (
