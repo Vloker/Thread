b499b3a0e018859d7b096d2034d9d8c73d6a7c86
Vloker <agimgimnastiar09@gmail.com>
2024-06-15 00:22:51 +0800

    update button

--- src/components/ButtonThread.jsx
+++ src/components/ButtonThread.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 function ButtonThread() {
 
-  const navigate = useNavigate()
+  const handleClick = () => {
+    Navigate('/thread')
+  }
 
   return (
     <button 
         className='bg-blue-800 hover:bg-blue-700 rounded-full text-white text-2xl w-16 h-16 z-20 fixed bottom-0 right-0'
-        onClick={() => navigate('/thread')}>+</button>
+        onClick={handleClick}>+</button>
   )
 }
 
